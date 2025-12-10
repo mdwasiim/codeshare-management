1281fa3cddf5f8ad6a37813083c570864e1cd031
Waseem <mdwasiim@gmail.com>
2025-12-10 08:41:15 -0800

    WIP: Before Angular 21 upgrade and Sakai migration

--- front-end/src/app/layout/default-layout/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './csm-footer/csm-footer.component';
-export * from './csm-header/csm-header.component';
-export * from './csm-layout.component';
--- front-end/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://dummyjson.com', // replace
-  useMockAuth: true // set false to call real API
-};
