b79a6889a7ef153e0239d5545af5a83193dc58e0
Waseem <mdwasiim@gmail.com>
2026-01-26 13:15:47 -0800

    Added Response handler

---
+++ back-end/services/auth-identity-service/src/main/java/com/codeshare/airline/auth/common/CSMResponseHandler.java
@@ -0,0 +1,38 @@
+package com.codeshare.airline.auth.common;
+
+import com.codeshare.airline.core.response.CSMServiceResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class CSMResponseHandler implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(
+            MethodParameter returnType,
+            Class<? extends HttpMessageConverter<?>> converterType
+    ) {
+        return returnType.hasMethodAnnotation(CSMResponse.class)
+                || returnType.getContainingClass().isAnnotationPresent(CSMResponse.class);
+    }
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter returnType,
+            MediaType selectedContentType,
+            Class<? extends HttpMessageConverter<?>> selectedConverterType,
+            ServerHttpRequest request,
+            ServerHttpResponse response
+    ) {
+        if (body instanceof CSMServiceResponse) {
+            return body;
+        }
+        return CSMServiceResponse.success(body);
+    }
+}
